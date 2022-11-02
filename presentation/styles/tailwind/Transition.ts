065b28bcf6dbe807ed66595b5645fbb16b78a916
ichihatsu1 <ichihatsu1@gmail.com>
2022-11-02 10:01:50 +0700

    Adding Style in Transition.ts & circle-bg

--- presentation/styles/tailwind/Transition.ts
+++ presentation/styles/tailwind/Transition.ts
@@ -1,1 +1,3 @@
-export const tailwindTransition = `transition-all ease-in-out duration-300`
+export const tailwindTransition = `transition-all ease-in-out duration-1000`
+export const pinkGradientText = `text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600`
+export const pinkGradientBg = `bg-gradient-to-r from-purple-400 to-pink-600`
