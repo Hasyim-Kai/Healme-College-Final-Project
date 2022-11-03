99afa0f24d6cc43c2c5766da40fb829fb27c9d96
Hasyim-Kai <ichihatsu1@gmail.com>
2022-11-03 10:08:41 +0700

    Finishing Landing Page

--- presentation/components/features/landing-page/Hero.tsx
+++ presentation/components/features/landing-page/Hero.tsx
@@ -6,18 +6,18 @@ import LoginBtn from './LoginBtn'
 export default function Hero() {
   return <section className='flex justify-around items-center py-5 px-7 text-6xl flex-wrap relative'>
     <article className='flex flex-col items-center'>
-        <div>
-          <p className='text-gray-600'>Heal Yourself</p>
-          <p className='text-gray-600'>Bloom your</p>
-          <p className={`${pinkGradientText} font-bold text-7xl py-2`}>Happiness</p>
-        </div>
-        <LoginBtn/>
+      <div>
+        <p className='text-gray-600'>Heal Yourself</p>
+        <p className='text-gray-600'>Bloom your</p>
+        <p className={`${pinkGradientText} font-bold text-7xl py-2`}>Happiness</p>
+      </div>
+      <LoginBtn />
     </article>
-    
+
     <div className=''>
-        <Image src="/smile1.png" alt="Smiling Woman" width={500} height={500}/>
+      <Image src="/smile1.png" alt="Smiling Woman" width={500} height={500} />
     </div>
 
-    <CircleBgAnimation/>    
+    <CircleBgAnimation />
   </section>
 }
