9f3dcd1102571c484fe85d4069147f6da4305721
Hasyim-Kai <ichihatsu1@gmail.com>
2022-11-05 10:25:06 +0700

    Refactor Violet Btn

--- presentation/components/global/VioletButton.tsx
+++ presentation/components/global/VioletButton.tsx
@@ -1,10 +1,11 @@
 import { pinkGradientBg, tailwindTransition } from "../../styles/TailwindStyle"
 
-type Props = { children: JSX.Element | JSX.Element[] | null, text?: string }
+type Props = { children?: JSX.Element | JSX.Element[] | null, text?: string, clickFunc: Function, isCenter?: boolean }
 
-export default function VioletButton({ text = 'Submit', children }: Props) {
-  return <button className={`flex gap-5 px-4 py-3 pt-1 text-white rounded-lg text-3xl shadow-lg my-7 hover:scale-110 hover:shadow-xl ${pinkGradientBg} ${tailwindTransition}`}>
-    {children}
-    <span>{text}</span>
+export default function VioletButton({ text = 'Submit', children, clickFunc, isCenter = false }: Props) {
+  return <button className={`flex gap-3 px-3 py-2 text-white rounded-lg text-lg shadow-lg hover:scale-110
+  hover:shadow-xl ${pinkGradientBg} ${tailwindTransition} ${isCenter ? 'mx-auto' : ''}`}
+    onClick={() => clickFunc()}>
+    {children} {text}
   </button>
 }
