217b3efaa68ae7861bb0b0cff4eeea63897f3c14
Hasyim-Kai <ichihatsu1@gmail.com>
2022-11-05 08:37:32 +0700

    Edit Cards Components Style

--- presentation/components/features/dashboard/circle/CircleCard.tsx
+++ presentation/components/features/dashboard/circle/CircleCard.tsx
@@ -1,7 +1,7 @@
 import { cardHover, pinkGradientBg } from '../../../../styles/TailwindStyle'
 
 export default function CircleCard() {
-  return <div className={`text-center rounded-full shadow-lg shadow-rose-200 h-40 bg-white bg-opacity-10 backdrop-blur-lg mx-5 lg:mx-0 ${cardHover}`}>
+  return <div className={`text-center rounded-full shadow-lg shadow-rose-200 h-40 bg-white bg-opacity-10 backdrop-blur-lg ${cardHover}`}>
     <p className={`text-2xl font-medium mt-6`}>Name</p>
     <div className={`w-2/3 h-1 rounded-lg mx-auto ${pinkGradientBg}`}></div>
     <p className={`text-sm mb-1`}>capacity</p>
--- presentation/components/features/dashboard/journal/JournalCard.tsx
+++ presentation/components/features/dashboard/journal/JournalCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { cardHover } from '../../../../styles/TailwindStyle'
 
 export default function JournalCard() {
@@ -7,26 +8,28 @@ export default function JournalCard() {
   const circumference = () => normalizedRadius() * 2 * Math.PI;
   const strokeDashoffset = () => circumference() - progress / 100 * circumference();
 
-  return <div className={`flex items-center rounded-lg shadow-lg h-36 bg-white bg-opacity-10 backdrop-blur-lg mx-5 lg:mx-0 ${cardHover}`}>
-    <div className='p-3 relative'>
-      <h1 className='absolute top-10 left-11 text-2xl drop-shadow-lg text-gray-600'>{progress}</h1>
-      <svg height={radius * 2} width={radius * 2}>
-        <circle
-          stroke="orchid"
-          fill="transparent"
-          strokeWidth={stroke}
-          strokeDasharray={circumference() + ' ' + circumference()}
-          style={{ strokeDashoffset: strokeDashoffset() }}
-          r={normalizedRadius()}
-          strokeLinecap="round"
-          cx={radius} cy={radius} />
-      </svg>
-    </div>
+  return <Link href={`journal/1`}>
+    <div className={`flex items-center rounded-lg shadow-lg h-36 bg-white bg-opacity-10 backdrop-blur-lg ${cardHover}`}>
+      <div className='p-3 relative'>
+        <h1 className='absolute top-10 left-11 text-2xl drop-shadow-lg text-gray-600'>{progress}</h1>
+        <svg height={radius * 2} width={radius * 2}>
+          <circle
+            stroke="orchid"
+            fill="transparent"
+            strokeWidth={stroke}
+            strokeDasharray={circumference() + ' ' + circumference()}
+            style={{ strokeDashoffset: strokeDashoffset() }}
+            r={normalizedRadius()}
+            strokeLinecap="round"
+            cx={radius} cy={radius} />
+        </svg>
+      </div>
 
-    <section className=''>
-      <p className={`text-sm mb-1`}><i>November 12nd 2023</i></p>
-      <p className={`text-2xl font-medium`}>Title</p>
-      <p className={`text-sm`}>Desc</p>
-    </section>
-  </div>
+      <section className=''>
+        <p className={`text-sm mb-1`}><i>November 12nd 2023</i></p>
+        <p className={`text-2xl font-medium`}>Title</p>
+        <p className={`text-sm`}>Desc</p>
+      </section>
+    </div>
+  </Link>
 }
