ad9a044b801769f4baf76da952a38e543a86ccd8
Hasyim-Kai <ichihatsu1@gmail.com>
2022-11-11 09:29:56 +0700

    Add Journal Detail data in Journal Detail Page

--- presentation/components/features/dashboard/journal/JournalDetail.tsx
+++ presentation/components/features/dashboard/journal/JournalDetail.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image'
 import { toggleModal } from '../../../../../app/GlobalSlice'
-import { useAppDispatch } from '../../../../../app/store'
+import { useAppDispatch, useAppSelector } from '../../../../../app/store'
 import VioletButton from "../../../global/VioletButton";
 import { cardHover, pinkGradientText } from '../../../../styles/TailwindStyle'
 import FloatBottomBtn from '../../../global/FloatBottomBtn'
 import UserNavbar from '../UserNavbar'
 import JournalFormModal from './JournalFormModal'
+import { formatDate } from '../../../../utils/DateFormatter';
+import { getJournals, selectJournalState } from '../../../../../app/JournalSlice';
 
 type Props = { journalId: string | string[] }
 
 export default function JournalDetail({ journalId = `1` }: Props) {
   const dispatch = useAppDispatch();
+  const journalState = useAppSelector(selectJournalState);
 
   return <div className='mx-auto lg:max-w-5xl'>
     <section className='mt-10'>
@@ -19,38 +22,19 @@ export default function JournalDetail({ journalId = `1` }: Props) {
         <div className='flex items-center gap-3'>
           <Image src="/icons/orchid-bookmark.svg" alt="Book Icons" width={60} height={60} />
           <div>
-            <h1 className={`text-5xl font-semibold ${pinkGradientText}`}>Title {journalId}</h1>
-            <h1 className='text-lg text-gray-500'><i>date</i></h1>
+            <h1 className={`text-5xl font-semibold ${pinkGradientText}`}>{journalState.journalDetail.title}</h1>
+            <h1 className='text-lg text-gray-500'><i>{formatDate(journalState.journalDetail.date)}</i></h1>
           </div>
         </div>
 
         <div className='flex items-center gap-3'>
-          <h1 className={`text-6xl font-semibold ${pinkGradientText}`}>80</h1>
+          <h1 className={`text-6xl font-semibold ${pinkGradientText}`}>{journalState.journalDetail.mood}</h1>
           <h1 className='text-lg text-gray-500'>Mood<br />Score</h1>
         </div>
       </div>
 
       <article className={`flex flex-col mt-5 mb-16 rounded-xl shadow-xl p-10 bg-white bg-opacity-20 backdrop-blur-lg mx-5 lg:mx-0`}>
-        <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Natus similique alias repudiandae, unde dolor, pariatur incidunt fugit
-          architecto accusantium impedit voluptatem. Ad eius similique recusandae
-          assumenda incidunt, aperiam ex maxime.</p>
-        <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Natus similique alias repudiandae, unde dolor, pariatur incidunt fugit
-          architecto accusantium impedit voluptatem. Ad eius similique recusandae
-          assumenda incidunt, aperiam ex maxime.</p>
-        <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Natus similique alias repudiandae, unde dolor, pariatur incidunt fugit
-          architecto accusantium impedit voluptatem. Ad eius similique recusandae
-          assumenda incidunt, aperiam ex maxime.</p>
-        <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Natus similique alias repudiandae, unde dolor, pariatur incidunt fugit
-          architecto accusantium impedit voluptatem. Ad eius similique recusandae
-          assumenda incidunt, aperiam ex maxime.</p>
-        <p className='text-lg'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Natus similique alias repudiandae, unde dolor, pariatur incidunt fugit
-          architecto accusantium impedit voluptatem. Ad eius similique recusandae
-          assumenda incidunt, aperiam ex maxime.</p>
+        <p className='text-lg'>{journalState.journalDetail.text}</p>
 
         <button className='mx-auto mt-7 p-2 rounded-full border-2 border-rose-300' onClick={() => console.log(`Jalan :)`)}>
           <Image src="/icons/trash.svg" alt="trash Icons" width={35} height={30} />
