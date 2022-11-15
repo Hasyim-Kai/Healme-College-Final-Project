2404152d39da72fcc0bf7952b18333abe700f410
Hasyim-Kai <ichihatsu1@gmail.com>
2022-11-15 14:32:23 +0700

    Add Schedule Functionality for Counselor

--- presentation/components/features/dashboard/consultation/ScheduleForm.tsx
+++ presentation/components/features/dashboard/consultation/ScheduleForm.tsx
@@ -6,13 +6,13 @@ import { useAppDispatch, useAppSelector } from '../../../../../app/store';
 import { selectUserState } from '../../../../../app/UserSlice';
 import { counselingSchedule } from '../../../../model/counseling-schedule';
 import { glassCard, pinkGradientText, simpleInput } from '../../../../styles/TailwindStyle';
-import UserNavbar from '../UserNavbar';
 import SessionCard from './SessionCard';
 
 type Props = { scheduleId: string | string[], isEdit?: boolean }
 
 export default function ScheduleForm({ scheduleId = `1`, isEdit = false }: Props) {
   const router = useRouter()
+  const { id } = router.query
   const dispatch = useAppDispatch();
   const { userInfo } = useAppSelector(selectUserState);
   const scheduleState = useAppSelector(selectScheduleState);
@@ -21,10 +21,17 @@ export default function ScheduleForm({ scheduleId = `1`, isEdit = false }: Props
   const handleGmeetLink = (event: any) => { setGmeetLink(event.target.value) }
   const [session, setSession] = useState<string>('1');
   const handleRadio = (event: any) => { setSession(event.target.value) }
+
   const goToMySchedule = () => { router.push('/counselor/counseling') }
   const handleSubmit = async (event: any) => {
     event.preventDefault()
-    await dispatch(createSchedule({ counselor_email: userInfo.email, gmeetLink, session }))
+    const type = event.nativeEvent.submitter.id
+    if (id !== `edit`) {
+      await dispatch(createSchedule({ counselor_email: userInfo.email, counselor_name: userInfo.name, gmeetLink, session }))
+    } else if (id === `edit`) {
+      // await dispatch(createSchedule({ counselor_email: userInfo.email, counselor_name: userInfo.name, gmeetLink, session }))
+      console.log({ counselor_email: userInfo.email, counselor_name: userInfo.name, gmeetLink, session })
+    }
     goToMySchedule()
   }
 
@@ -35,14 +42,14 @@ export default function ScheduleForm({ scheduleId = `1`, isEdit = false }: Props
       <form className={`flex flex-col mt-5 mb-16 rounded-xl shadow-xl p-10 mx-5 lg:mx-0 ${glassCard}`} onSubmit={handleSubmit}>
         <div>
           <label htmlFor="Gmeet">Gmeet Link</label><br />
-          <input className={`bg-transparent ${simpleInput}`} type="text" name="Gmeet" id="Gmeet" placeholder="Enter Gmeet Link here" required />
+          <input className={`bg-transparent ${simpleInput}`} type="text" name="Gmeet" id="Gmeet" placeholder="Enter Gmeet Link here" onChange={handleGmeetLink} required />
         </div>
 
         <ul className="mt-5 grid gap-6 w-full grid-cols-1 lg:grid-cols-3">
           {counselingSchedule.map((item: any) => <SessionCard value={item.value} text={item.text} desc={item.desc} checkedRadio={session} onCangeFunc={handleRadio} key={item.value} />)}
         </ul>
 
-        <button className='mt-12'>
+        <button className='mt-12' id='create'>
           <Image src="/icons/orchid-circle-check.svg" alt="trash Icons" width={60} height={60} />
         </button>
       </form>
