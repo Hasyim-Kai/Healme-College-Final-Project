44d1242d9565a50e978962b319dbd2242be24e0f
Kai-69 <ichihatsu1@gmail.com>
2023-03-10 14:40:17 +0700

    Fixing landing page margin for mobile

--- presentation/components/features/landing-page/Features.tsx
+++ presentation/components/features/landing-page/Features.tsx
@@ -7,7 +7,7 @@ export default function Features() {
         <div className={`w-52 h-2 mx-auto mt-3 mb-16 drop-shadow-lg rounded-lg ${pinkGradientBg}`}></div>
 
         <section className='mb-14 flex flex-wrap justify-center items-center lg:gap-24 my-5'>
-            <div className='relative w-96 aspect-square rounded-lg shadow-lg overflow-hidden'>
+            <div className='mx-5 lg:mx-0 relative w-96 aspect-square rounded-lg shadow-lg overflow-hidden'>
                 <Image src="/img/consultation.png" alt="consultation" layout='fill' objectFit='fill' placeholder="blur" blurDataURL='/img/blur-placeholder.png' priority />
             </div>
             <div className='max-w-xs'>
@@ -18,7 +18,7 @@ export default function Features() {
         </section>
 
         <section className='mb-14 flex flex-wrap lg:flex-row-reverse justify-center items-center lg:gap-24 my-5'>
-            <div className='relative w-96 aspect-square rounded-lg shadow-lg overflow-hidden'>
+            <div className='mx-5 lg:mx-0 relative w-96 aspect-square rounded-lg shadow-lg overflow-hidden'>
                 <Image src="/img/journal.png" alt="journal" layout='fill' objectFit='fill' placeholder="blur" blurDataURL='/img/blur-placeholder.png' priority />
             </div>
             <div className='max-w-xs'>
@@ -29,7 +29,7 @@ export default function Features() {
         </section>
 
         <section className='mb-24 flex flex-wrap justify-center items-center lg:gap-24 my-5'>
-            <div className='relative w-96 aspect-square rounded-lg shadow-lg overflow-hidden'>
+            <div className='mx-5 lg:mx-0 relative w-96 aspect-square rounded-lg shadow-lg overflow-hidden'>
                 <Image src="/img/circle.png" alt="Circle" layout='fill' objectFit='fill' placeholder="blur" blurDataURL='/img/blur-placeholder.png' priority />
             </div>
             <div className='max-w-xs'>
