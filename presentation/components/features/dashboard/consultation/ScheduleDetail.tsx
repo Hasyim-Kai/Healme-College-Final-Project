d0a7515584664f738b881805cd4cd7af6e3fac52
Hasyim-Kai <muhammad.hasyim.c.a@gmail.com>
2023-06-22 17:31:50 +0700

    Add dummy UI for "addNote" in scheudle detail

--- presentation/components/features/dashboard/consultation/ScheduleDetail.tsx
+++ presentation/components/features/dashboard/consultation/ScheduleDetail.tsx
@@ -10,8 +10,8 @@ import { checkIsBooked, delSchedule, selectScheduleState } from '../../../../../
 import { counselingSchedule } from '../../../../model/counseling-schedule';
 import { formatDate } from '../../../../utils/DateFormatter';
 import { useEffect } from 'react';
-import { userInfo } from 'os';
 import { selectUserState } from '../../../../../app/UserSlice';
+import CounselorScheduleFormModal from './CounselorScheduleFormModal';
 
 type Props = { scheduleId: string | string[], isCounselor?: boolean }
 
@@ -54,31 +54,41 @@ export default function ScheduleDetail({ scheduleId = `1`, isCounselor = false }
         {(!scheduleState.isBooked && !isCounselor) && <button className={`flex justify-center p-5 rounded-xl shadow-xl ${glassCard}`} onClick={() => dispatch(toggleModal())}>
           <span className={`text-3xl font-semibold ${pinkGradientText}`}>Apply Here</span>
         </button>}
+
+        {(!isCounselor) && <button className={`flex justify-center p-5 rounded-xl shadow-xl ${glassCard}`} onClick={() => dispatch(toggleModal())}>
+          <span className={`text-3xl font-semibold ${pinkGradientText}`}>Add Note</span>
+        </button>}
       </div>
 
-      <article className={`col-span-3 flex flex-col rounded-xl shadow-xl p-10 mx-5 lg:mx-0 ${glassCard}`}>
-        <div className={`flex jusb items-center gap-5 mb-5`}>
-          {scheduleState.scheduleDetail?.patient_name === undefined ? <h1 className='text-xl font-semibold'><i>Not Applied yet</i></h1>
-            : <><h1 className='text-3xl font-semibold'>By {scheduleState.scheduleDetail?.patient_name}</h1>
-              <Link href={isCounselor ? `/counselor/journal` : `/user/journal`}>
-                <button className='p-2 rounded-full border-2 border-rose-300'>
-                  <Image src="/icons/orchid-book.svg" alt="orchid-book Icons" width={35} height={30} />
-                </button>
-              </Link></>}
-        </div>
+      <div className='col-span-3 flex flex-col gap-5'>
+        <article className={`flex flex-col rounded-xl shadow-xl p-10 mx-5 lg:mx-0 ${glassCard}`}>
+          <div className={`flex jusb items-center gap-5 mb-5`}>
+            {scheduleState.scheduleDetail?.patient_name === undefined ? <h1 className='text-xl font-semibold'><i>Not Applied yet</i></h1>
+              : <><h1 className='text-3xl font-semibold'>By {scheduleState.scheduleDetail?.patient_name}</h1>
+                <Link href={isCounselor ? `/counselor/journal` : `/user/journal`}>
+                  <button className='p-2 rounded-full border-2 border-rose-300'>
+                    <Image src="/icons/orchid-book.svg" alt="orchid-book Icons" width={35} height={30} />
+                  </button>
+                </Link></>}
+          </div>
 
-        <p className='text-lg'>{scheduleState.scheduleDetail?.summary}</p>
+          <p className='text-lg'>{scheduleState.scheduleDetail?.summary}</p>
 
-        {isCounselor && <button className='mx-auto mt-12 p-2 rounded-full border-2 border-rose-300' onClick={handleDel}>
-          <Image src="/icons/trash.svg" alt="trash Icons" width={35} height={30} />
-        </button>}
-      </article>
+          {isCounselor && <button className='mx-auto mt-12 p-2 rounded-full border-2 border-rose-300' onClick={handleDel}>
+            <Image src="/icons/trash.svg" alt="trash Icons" width={35} height={30} />
+          </button>}
+        </article>
+
+        <article className={`flex flex-col rounded-xl shadow-xl p-10 mx-5 lg:mx-0 ${glassCard}`}>
+          <p className='text-lg'>{scheduleState.scheduleDetail?.summary}</p>
+        </article>
+      </div>
 
     </section>
 
     {isCounselor && <FloatBottomBtn text='Edit' clickFunc={goToForm}>
       <Image src="/icons/edit-note.svg" alt="edit note Icons" width={25} height={25} />
     </FloatBottomBtn>}
-    {!isCounselor && <UserScheduleFormModal isEdit={true} />}
+    {isCounselor ? <CounselorScheduleFormModal /> : <UserScheduleFormModal isEdit={true} />}
   </div>
 }
